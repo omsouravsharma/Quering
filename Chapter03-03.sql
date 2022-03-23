a57bc81919ddf65cfad3d238dbba498a620a349a
omsouravsharma <souravsharma.sep22@gmail.com>
2022-03-24 03:25:05 +0530

    330

--- Chapter03-03.sql
+++ Chapter03-03.sql
@@ -122,3 +122,60 @@ USE TSQLV3
 --WHERE O.orderid IS not NULL;
 
 --pg -320
+
+-- ALGORITHM NESTED, MERGE, HASH
+
+--USE PERFORMANCEV3
+
+--SELECT C.custid, C.custname, O.orderid, O.empid, O.shipperid, O.orderdate
+--FROM DBO.CUSTOMERS AS C
+--INNER JOIN DBO.ORDERS AS O
+--ON O.CUSTID = C.CUSTID
+--WHERE C.CUSTNAME LIKE 'Cust_1000%'
+--AND O.orderdate >='20140101' AND O.orderdate < '20140401'
+
+
+--CREATE INDEX IDX_NC_CN_I_CID on DBO.CUSTOMERS(CUSTNAME) INCLUDE (CUSTID);
+--CREATE INDEX idx_nc_cid_od_i_oid_eid_sid ON DBO.ORDERS(custid, orderdate) include(orderid, empid, shipperid);
+
+--SET NOCOUNT ON;
+USE tempdb;
+--IF OBJECT_ID(N'dbo.Arrays', N'U') IS NOT NULL DROP TABLE dbo.Arrays;
+--CREATE TABLE dbo.Arrays
+--(
+--id VARCHAR(10) NOT NULL PRIMARY KEY,
+--arr VARCHAR(8000) NOT NULL
+--);
+--GO
+--INSERT INTO dbo.Arrays VALUES('A', '20,223,2544,25567,14');
+--INSERT INTO dbo.Arrays VALUES('B', '30,-23433,28');
+--INSERT INTO dbo.Arrays VALUES('C', '12,10,8099,12,1200,13,12,14,10,9');
+--INSERT INTO dbo.Arrays VALUES('D', '-4,-6,-45678,-2');
+
+--SELECT * FROM DBO.ARRAYS
+
+--SELECT id, 
+--ROW_NUMBER() over (PARTITION BY ID ORDER BY N)  AS POS, 
+--SUBSTRING(ARR, N, CHARINDEX(',', ARR+ ',',N)-N) AS ELEMENT, 
+--arr, n
+--FROM dbo.Arrays
+--INNER JOIN TSQLV3.dbo.Nums
+--ON n <= LEN(arr) +1
+--AND SUBSTRING(',' + arr, n, 1) = ',';
+
+
+--alter FUNCTION DBO.SPLITS(@ARR AS VARCHAR(8000), @SEP AS CHAR(1)) RETURNS TABLE
+--AS 
+--RETURN
+
+--SELECT 
+--ROW_NUMBER() over (ORDER BY N)  AS POS, 
+--SUBSTRING(@ARR, N, CHARINDEX(@SEP, @ARR+ @SEP,N)-N) AS ELEMENT
+--FROM TSQLV3.dbo.Nums
+--WHERE n <= LEN(@ARR) +1
+--AND SUBSTRING(@SEP + @ARR, n, 1) = @SEP;
+--GO
+
+
+
+--SELECT * FROM dbo.SplitS('10248,10249,10250', ',') AS S;
