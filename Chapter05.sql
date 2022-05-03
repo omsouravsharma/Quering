00b8ea01d06bb9507a389194326ad9b13c66fbac
omsouravsharma <souravsharma.sep22@gmail.com>
2022-05-03 23:02:16 +0530

    pg442

--- Chapter05.sql
+++ Chapter05.sql
@@ -1,1 +1,45 @@
 -- CHAPTER 5 TOP AND OFFSET-FETCH
+-- TOP AND OFFSET-FETCH FILTERS
+
+-- TOP FILTERS
+
+USE TSQLV3;
+SELECT TOP (3) orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+USE TSQLV3;
+SELECT TOP (1) PERCENT orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+--WITH TIES
+
+SELECT TOP (3) WITH TIES orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+SELECT TOP (1) WITH TIES orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY ROW_NUMBER() OVER(PARTITION BY custid ORDER BY orderdate DESC, orderid
+DESC);
+
+SELECT TOP (3) orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC, orderid DESC;
+
+SELECT TOP (3) orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY (SELECT NULL)
+
+-- OFFSET-FETCH FILTER
+
+SELECT orderid, orderdate, custid, empid
+FROM Sales.Orders
+ORDER BY orderdate DESC, orderid DESC
+OFFSET 50 ROWS FETCH NEXT 25 ROWS ONLY;
+
+--HETCH IS OPTIONAL
+
+-- OPTIMIZATION OF FILTERS DEMOSTRATED THROUGH PAGIN
+--PG442
