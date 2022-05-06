eea2151ba002994f00ed3ff817f308f7a5d0be9e
omsouravsharma <souravsharma.sep22@gmail.com>
2022-05-06 23:57:47 +0530

    460

--- Chapter05.sql
+++ Chapter05.sql
@@ -86,3 +86,123 @@ EXEC dbo.GetPage @pagesize = 25;
 EXEC dbo.GetPage @orderdate = '20101207', @orderid = 410, @pagesize = 25;
 
 --OPTIMITION OF OFFSET-FETCH PG 451
+
+IF OBJECT_ID('DBO.GETPAGE', 'P') IS NOT NULL DROP PROC DBO.GetPage;
+GO
+CREATE PROC DBO.GETPAGE
+@PAGENUM AS BIGINT= 1,
+@PAGESIZE AS BIGINT = 25
+AS 
+
+SELECT ORDERID, ORDERDATE, CUSTID, EMPID
+FROM DBO.Orders
+ORDER BY ORDERID
+OFFSET(@PAGENUM -1) * @PAGESIZE ROWS FETCH NEXT @PAGESIZE ROWS ONLY
+GO
+
+
+EXEC dbo.GetPage @pagenum = 1, @pagesize = 25;
+EXEC dbo.GetPage @pagenum = 2, @pagesize = 25;
+EXEC dbo.GetPage @pagenum = 3, @pagesize = 25;
+
+
+ALTER PROC dbo.GetPage
+@pagenum AS BIGINT = 1,
+@pagesize AS BIGINT = 25
+AS
+WITH K AS
+(
+SELECT orderid
+FROM dbo.Orders
+ORDER BY orderid
+OFFSET (@pagenum - 1) * @pagesize ROWS FETCH NEXT @pagesize ROWS ONLY
+)
+SELECT O.orderid, O.orderdate, O.custid, O.empid
+FROM dbo.Orders AS O
+INNER JOIN K
+ON O.orderid = K.orderid
+ORDER BY O.orderid;
+GO
+
+EXEC dbo.GetPage @pagenum = 1000, @pagesize = 25;
+
+
+--OFFSET TO | AFTER 
+
+SELECT orderid, orderdate, custid, empid
+FROM dbo.Orders
+ORDER BY orderdate, orderid
+OFFSET AFTER (@anchor_orderdate, @anchor_orderid) -- input anchor sort keys
+FETCH NEXT @pagesize ROWS ONLY
+LAST ROW INTO (@last_orderdate, @last_orderid); -- outputs for next page
+request
+
+
+
+
+--OPTIMIZATION OF ROW_NUMBER
+
+IF OBJECT_ID(N'dbo.GetPage', N'P') IS NOT NULL DROP PROC dbo.GetPage;
+GO
+CREATE PROC dbo.GetPage
+@pagenum AS BIGINT = 1,
+@pagesize AS BIGINT = 25
+AS
+WITH C AS
+(
+SELECT orderid, orderdate, custid, empid,
+ROW_NUMBER() OVER(ORDER BY orderid) AS rn
+FROM dbo.Orders
+)
+SELECT orderid, orderdate, custid, empid
+FROM C
+WHERE rn BETWEEN (@pagenum - 1) @pagesize + 1 AND @pagenum @pagesize
+ORDER BY rn; -- if order by orderid get sort in plan
+GO
+
+EXEC dbo.GetPage @pagenum = 1, @pagesize = 25;
+EXEC dbo.GetPage @pagenum = 2, @pagesize = 25;
+EXEC dbo.GetPage @pagenum = 3, @pagesize = 25;
+
+
+ALTER PROC dbo.GetPage
+@PAGENUM AS BIGINT= 1,
+@PAGESIZE AS BIGINT = 25
+AS
+WITH C AS
+(
+SELECT orderid, ROW_NUMBER() OVER(ORDER BY orderid) AS rn
+FROM dbo.Orders
+),
+K AS
+(
+SELECT orderid, rn
+FROM C
+WHERE rn BETWEEN (@pagenum - 1) @PAGESIZE + 1 AND @pagenum @pagesize
+)
+SELECT O.orderid, O.orderdate, O.custid, O.empid
+FROM dbo.Orders AS O
+INNER JOIN K
+ON O.orderid = K.orderid
+ORDER BY K.rn;
+GO
+
+USE PerformanceV3;
+IF OBJECT_ID(N'dbo.MyOrders', N'U') IS NOT NULL DROP TABLE dbo.MyOrders;
+GO
+SELECT * INTO dbo.MyOrders FROM dbo.Orders;
+CREATE UNIQUE CLUSTERED INDEX idx_od_oid ON dbo.MyOrders(orderdate, orderid);
+
+DELETE TOP (50) FROM dbo.MyOrders;
+
+
+WITH C AS
+(
+SELECT TOP (50) *
+FROM dbo.MyOrders
+ORDER BY orderdate, orderid
+)
+DELETE FROM C;
+
+
+--MODIFYING IN CHUNKS PG 460
