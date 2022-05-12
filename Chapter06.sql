84dd50ef86d1194f384b1d4218c0e713be75d48a
omsouravsharma <souravsharma.sep22@gmail.com>
2022-05-12 23:09:11 +0530

    476

--- Chapter06.sql
+++ Chapter06.sql
@@ -19,3 +19,18 @@ select COUNT(*)
 FROM DBO.ORDERS
 
 --pg 476
+
+USE PerformanceV3;
+IF OBJECT_ID(N'dbo.MyOrders', N'U') IS NOT NULL DROP TABLE dbo.MyOrders;
+
+
+USE PerformanceV3;
+BEGIN TRAN
+SELECT orderid, custid, empid, shipperid, orderdate, filler
+INTO dbo.MyOrders
+FROM dbo.Orders;
+
+USE PerformanceV3;
+SELECT SCHEMA_NAME(schema_id) AS schemaname, name AS tablename FROM sys.tables;
+
+--pg 476
